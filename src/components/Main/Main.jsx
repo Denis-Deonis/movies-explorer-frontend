6d6d19fc2ef158487925431a088e4a67068910de
Denis Zykov <denis.zykov.1993@gmail.com>
2023-07-22 16:22:07 +0300

    feat: add project

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import Promo from "./Promo/Promo";
 import Header from "../Header/Header";
+import Project from "./Project/Project";
 
 export default function Main() {
   return (
     <div >
       <Header />
       <Promo/>
+      <Project/>
     </div>
   )
 }
