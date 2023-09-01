063fa58686a36c38689a2ed14d534c2fbc15e322
Denis Zykov <denis.zykov.1993@gmail.com>
2023-09-01 14:50:19 +0300

    hotfix: change the number of cards

--- src/utils/visibleCount.js
+++ src/utils/visibleCount.js
@@ -1,13 +1,12 @@
-
 export const getVisibleCount = () => {
   const width = window.innerWidth;
   if (width < 653) {
     return {loadCards: 5, moreCards: 2}
   } else if (width >= 653 && width < 767) {
     return {loadCards: 8, moreCards: 2}
-  } else if (width > 767) {
+  } else if (width >= 767 && width < 1200) {
     return {loadCards: 8, moreCards: 2}
-  }else if (width > 1279) {
+  } else if (width >= 1200) {
     return {loadCards: 12, moreCards: 3}
   }
 }
